33088af5769f99e193173624286fb50eb124aca4
jianviper <linjianviper@vip.qq.com>
2016-09-22 21:40:12 +0800

    templates

--- part1/g.py
+++ part1/g.py
@@ -3,3 +3,7 @@ from hello import app
 from flask import current_app
 
 print app.url_map
+
+app_ctx=app.app_context()
+app_ctx.push()
+print current_app.__name__
--- part1/hello.py
+++ part1/hello.py
@@ -1,16 +1,34 @@
 # -*- coding:utf-8 -*-
-from flask import Flask
-from flask import request
+from flask import Flask,request,make_response,redirect,abort,render_template
+from flask_script import Manager
 
 app = Flask(__name__)
+#manager = Manager(app)
 
 @app.route('/')
 def index():
-	return '<h1>hello world!</h1>'
+	response = make_response('<h1>hello world!</h1>')
+	response.set_cookie('name','linjian')
+	#return redirect('http://www.baidu.com')
+	#return  response
+	return render_template('index.html')
 
-@app.route('/user/<name>')
+@app.route('/user/<name>/')
 def user(name):
-	return '<h1>hello, %s!</h1>' % name
+	#return '<h1>hello, %s!</h1>' % name
+	return render_template('user.html',name=name)
+
+@app.route('/user_id/<int:id>')
+def get_user(id):
+	if id != 1:
+		abort(404)
+	return '<h1>hello,%d!' % id
+
+@app.route('/list/')
+def lists():
+	lists = ['a','b','c','d',]
+	return render_template('list.html',lists=lists)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0',debug=True)
+	#manager.run()
