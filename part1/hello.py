614ee2b65c401dfd02d68eae2889934b8f122ca7
jianviper <linjianviper@vip.qq.com>
2016-10-12 20:45:07 +0800

    base,404,500,ico
    modify:base,user
    add:404,500,*.ico

--- part1/hello.py
+++ part1/hello.py
@@ -1,14 +1,16 @@
 # -*- coding:utf-8 -*-
 from flask import Flask,request,make_response,redirect,abort,render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-#manager = Manager(app)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-	response = make_response('<h1>hello world!</h1>')
-	response.set_cookie('name','linjian')
+	#response = make_response('<h1>hello world!</h1>')
+	#response.set_cookie('name','linjian')
 	#return redirect('http://www.baidu.com')
 	#return  response
 	return render_template('index.html')
@@ -29,6 +31,14 @@ def lists():
 	lists = ['a','b','c','d',]
 	return render_template('list.html',lists=lists)
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'),500
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',debug=True)
-	#manager.run()
+	#app.run(host='0.0.0.0',debug=True)
+	app.run(debug=True)
