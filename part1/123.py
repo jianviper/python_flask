7f67b6ed2bed206e2d607f69574e275c7e8bc435
jianviper <linjianviper@vip.qq.com>
2016-11-04 13:41:14 +0800

    修改进页面不显示session的name值

---
+++ part1/123.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+import time
+
+print time.time()
+time.sleep(1.34)
+print time.time()
---
+++ part1/main.py
@@ -0,0 +1,77 @@
+# -*- coding:utf-8 -*-
+import time
+from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+import wtforms as wtfs
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+	#response = make_response('<h1>hello world!</h1>')
+	#response.set_cookie('name','linjian')
+	#return redirect('http://www.baidu.com')
+	#return  response
+	return render_template('index.html')
+
+
+@app.route('/user/<name>/')
+def user(name):
+	#return '<h1>hello, %s!</h1>' % name
+	return render_template('user.html', name=name)
+
+
+@app.route('/user_id/<int:id>')
+def get_user(id):
+	if id != 1:
+		abort(404)
+	return '<h1>hello,%d!' % id
+
+
+@app.route('/list/')
+def lists():
+	lists = ['a', 'b', 'c', 'd', ]
+	return render_template('list.html', lists=lists)
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form1():
+	print 'session:%s' % session
+	form = NameFrom()
+	if form.validate_on_submit():
+		session['name'] = form.name.data
+		session['flag'] = time.time()
+		return redirect(url_for('form1'))
+	if 'flag' not in session.keys():
+		return render_template('form.html', form=form, name=None)
+	elif time.time() - float(session['flag']) > 1.0:
+		return render_template('form.html', form=form, name=None)
+
+	return render_template('form.html', form=form, name=session.get('name'))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500
+
+
+class NameFrom(FlaskForm):
+	name = wtfs.StringField('what is your name?', validators=[wtfs.validators.DataRequired()])
+	text = wtfs.StringField('', validators=[wtfs.validators.DataRequired()])
+	submit = wtfs.SubmitField('submit')
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True)
+#app.run(debug=True)
